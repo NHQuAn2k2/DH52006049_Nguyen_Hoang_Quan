fb44dc6cfe81487051d2b1b2e8974c66a11421e8
quan <mhoangquan0@gmail.com>
2023-11-11 16:33:46 +0700

    nhap so

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,20 @@
+#include "iostream";
+using namespace std;
+void kiemTraSoChinhPhuong() {}
+void kiemTraSoHoanThien(int s) {
+	int so1 = s - 1;
+	int so2 = s - 2;
+	if ((so1 + so2) == s) {
+		cout<<s<<" la so hoan thien";
+	}
+	else {
+		cout<<s<<" khong phai so hoan thien";
+	};
+}
+int main() {
+	int n;
+	cout << "nhap so nguyen: ";
+	cin >> n;
+	cout << 17 % 3;
+	kiemTraSoHoanThien(n);
+}
